67001112ce2e16ee8413774e716d88e866ea134e
Cindy Chu <cindychu619@yahoo.com>
2019-02-19 23:35:26 -0800

    created about page with carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Carousel.css";
+import Carousel from 'react-bootstrap/Carousel';
+
+const Carousel = () => (
+  <div className="carousel">
+    <span>This is a Carousel</span>
+  </div>
+);
+
+export default Carousel;
+
+
+
+
+
+
